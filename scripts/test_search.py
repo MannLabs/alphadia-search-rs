65d2632f2a9a8c304662fceba5ac6f01838231c6
GeorgWa <wallmann@biochem.mpg.de>
2025-05-05 01:38:08 +0200

    add test search

---
+++ scripts/test_search.py
@@ -0,0 +1,51 @@
+from alpha_rs import SpecLibFlat, test_xic_index, PeakGroupScoring
+import os
+import pandas as pd
+import numpy as np
+tmp_folder = "/Users/georgwallmann/Documents/data/alpha-rs"
+import logging
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+
+    logger = logging.getLogger(__name__)
+
+    logger.info("Loading data")
+    spectrum_df = pd.read_parquet(os.path.join(tmp_folder, 'spectrum_df.parquet'))
+    peak_df = pd.read_parquet(os.path.join(tmp_folder, 'peak_df.parquet'))
+
+    precursor_df = pd.read_parquet(os.path.join(tmp_folder, 'precursor_df.parquet'))
+    fragment_df = pd.read_parquet(os.path.join(tmp_folder, 'fragment_df.parquet'))
+
+    logger.info("Creating spec lib")
+
+    speclib = SpecLibFlat.from_arrays(
+        precursor_df['precursor_mz'].values.astype(np.float32),
+        precursor_df['flat_frag_start_idx'].values,
+        precursor_df['flat_frag_stop_idx'].values,
+        fragment_df['mz'].values.astype(np.float32),
+        fragment_df['intensity'].values.astype(np.float32)
+    )
+
+    logger.info("Creating rs data")
+    rs_data = test_xic_index(
+        spectrum_df['delta_scan_idx'].values,
+        spectrum_df['isolation_lower_mz'].values.astype(np.float32),
+        spectrum_df['isolation_upper_mz'].values.astype(np.float32),
+        spectrum_df['peak_start_idx'].values,
+        spectrum_df['peak_stop_idx'].values,
+        spectrum_df['cycle_idx'].values,
+        spectrum_df['rt'].values.astype(np.float32),
+        peak_df['mz'].values.astype(np.float32),
+        peak_df['intensity'].values.astype(np.float32)
+    )
+
+    logger.info("Creating peak group scoring")
+    fwhm_rt = 3
+    kernel_size = 20
+
+    peak_group_scoring = PeakGroupScoring(fwhm_rt, kernel_size)
+
+    logger.info("Searching")    
+    mass_tolerance = 7
+    peak_group_scoring.search(rs_data, speclib, mass_tolerance)
